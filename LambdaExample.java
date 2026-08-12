fd07cae925c7143066bee969f102a10ef8c2a00e
NadaBadawyy <nadabadawy776@gmail.com>
2026-08-12 19:51:06 +0300

    finish the task

---
+++ LambdaExample.java
@@ -0,0 +1,181 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class LambdaExample {
+
+    public static void main(final String[] args) {
+
+        final UsersRepository repository = new UsersRepository();
+
+        banner("Listing all users");
+        // SOLVED all users
+        repository.select(null, null);
+
+        banner("Listing all active users");
+        // SOLVED With functional interfaces declared
+        Predicate<User> activeUserPredicate = new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.active;
+            }
+        };
+        repository.select(activeUserPredicate, null);
+
+        banner("Listing all active users - lambda");
+        // SOLVED With functional interfaces used directly
+        repository.select(user -> user.active, null);
+        
+        banner("Listing users with age > 5 sorted by name");
+        // TODO With functional interfaces declared  >>> Done
+
+        Predicate<User>p1= new Predicate<User>(){
+
+            @Override
+            public boolean test(User t) {
+                return t.age>5;
+            
+        }};
+        Comparator<User> c1= new Comparator<User>() {
+
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.name.compareTo(o2.name);
+            }
+            
+        };
+        repository.select(p1,c1);
+
+
+        banner("Listing users with age > 5 sorted by name - lambda");
+        // TODO With functional interfaces used directly >>> Done
+
+
+        repository.select(user->user.age>5, (a,b)->a.name.compareTo(b.name));
+
+        banner("Listing users with age < 10 sorted by age");
+        // TODO With functional interfaces declared  >>> Done
+
+        Predicate<User>p2= new Predicate<User>() {
+
+            @Override
+            public boolean test(User t) {
+                return  t.age<10;
+            }
+
+        };
+        Comparator<User>c2= new Comparator<User>() {
+            @Override
+            public int compare(User u1, User u2){
+                return Integer.compare(u1.age,u2.age);
+            }
+              
+        };
+
+        repository.select(p2,c2);
+
+        banner("Listing users with age < 10 sorted by age - lambda");
+        // TODO With functional interfaces used directly  >>> Done
+
+        repository.select(user->user.age<10,(a,b)->Integer.compare(a.age, b.age));
+
+
+
+        banner("Listing active users sorted by name");
+        // TODO With functional interfaces declared  >>> Done
+        Predicate<User>p3= new Predicate<User>() {
+            @Override
+            public boolean test(User u){
+                return u.active;
+
+            }
+        };
+        Comparator<User>c3= new Comparator<User>() {
+            @Override 
+            public int compare(User u1, User u2){
+                return u1.name.compareTo(u2.name);
+            }
+        };
+        repository.select(p3, c3);
+
+
+        banner("Listing active users sorted by name - lambda");
+        // TODO With functional interfaces used directly  >>> Done
+        repository.select(user->user.active, (a,b)->a.name.compareTo(b.name));
+
+        banner("Listing active users with age > 8 sorted by name");
+        // TODO With functional interfaces declared   >>> Done
+
+        Predicate<User>p4 = new Predicate<User>() {
+            @Override
+            public boolean test (User u){
+                return u.active&&u.age>8;
+            }
+        };
+        Comparator<User>c4= new Comparator<User>() {
+            @Override
+            public int compare(User u1, User u2){
+                return u1.name.compareTo(u2.name);
+            }
+        };
+        repository.select(p4, c4);
+
+        banner("Listing active users with age > 8 sorted by name - lambda");
+        // TODO With functional interfaces used directly  >>> Done
+        repository.select(user->user.active && user.age>8,(a,b)->a.name.compareTo(b.name));
+
+    }
+
+    private static void banner(final String m) {
+        System.out.println("#### " + m + " ####");
+    }
+    
+}
+
+class UsersRepository {
+    List<User> users;
+
+    UsersRepository() {
+        users = Arrays.asList(
+            new User("Seven", 7, true),
+            new User("Four", 4, false),
+            new User("Eleven", 11, true),
+            new User("Three", 3, true),
+            new User("Nine", 9, false),
+            new User("One", 1, true),
+            new User("Twelve", 12, true));
+    }
+
+    void select(final Predicate<User> filter, final Comparator<User> order) {
+        Stream<User> usersStream = users.stream();
+
+        if (filter != null) {
+            usersStream = usersStream.filter(filter);
+        }
+        if (order != null) {
+            usersStream = usersStream.sorted(order);
+        }
+        usersStream.forEach(System.out::println);
+    }
+}
+
+class User {
+    String name;
+    int age;
+    boolean active;
+
+    User(final String name, final int age, boolean active) {
+        this.name = name;
+        this.age = age;
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\t| " + age;
+    }
+}
---
+++ StreamsExample.java
@@ -0,0 +1,200 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.ToIntFunction;
+import java.util.stream.Stream;
+
+public class StreamsExample {
+
+    public static void main(final String[] args) {
+
+        List<Author> authors = Library.getAuthors();
+
+        banner("Authors information");
+        // SOLVED With functional interfaces declared
+        Consumer<Author> authorPrintConsumer = new Consumer<Author>() {
+            @Override
+            public void accept(Author author) {
+                System.out.println(author);
+            }
+        };
+        authors
+                .stream()
+                .forEach(authorPrintConsumer);
+
+        // SOLVED With functional interfaces used directly
+        authors
+                .stream()
+                .forEach(System.out::println);
+
+        banner("Active authors");
+        // TODO With functional interfaces declared >>> Done
+
+        Predicate<Author> p1 = new Predicate<Author>() {
+            @Override
+            public boolean test(Author a) {
+                return a.active;
+            }
+        };
+        authors.stream()
+        .filter(p1)
+        .forEach(authorPrintConsumer);
+        banner("Active authors - lambda");
+        // TODO With functional interfaces used directly >>> Done
+        authors.stream()
+        .filter(author -> author.active)
+        .forEach(System.out::println);
+
+        banner("Active books for all authors");
+        // TODO With functional interfaces declared  >>> Done
+        Function<Author, Stream<Book>>booksStream=new Function<Author,Stream<Book>>() {
+
+            @Override
+            public Stream<Book> apply(Author t) {
+                return t.books.stream();
+            }
+            
+        };
+        Consumer<Book> booksPrinter= new Consumer<Book>() {
+            @Override
+            public void accept(Book b){
+                System.out.println(b);
+            }
+        };
+        
+  
+        authors.stream()
+        .filter(p1)
+        .flatMap(booksStream)
+        .forEach(booksPrinter);
+
+        banner("Active books for all authors - lambda");
+        // TODO With functional interfaces used directly >>> Done
+
+        authors.stream()
+        .filter(author->author.active)
+        .flatMap(author->author.books.stream())
+        .forEach(System.out::println);
+
+        banner("Average price for all books in the library");
+        // TODO With functional interfaces declared  >>> Done
+
+        ToIntFunction<Book> bookToPrice= new ToIntFunction<Book>() {
+
+            @Override
+            public int applyAsInt(Book value) {
+                return value.price;
+            }
+            
+        };
+        System.out.println(authors.stream()
+        .flatMap(booksStream)
+        .mapToInt(bookToPrice)
+        .average());
+        banner("Average price for all books in the library - lambda");
+        // TODO With functional interfaces used directly  >>> Done
+        System.out.println(authors.stream()
+        .flatMap(author->author.books.stream())
+        .mapToInt(book->book.price)
+        .average());
+
+
+        banner("Active authors that have at least one published book");
+        // TODO With functional interfaces declared  >>> Done
+        Predicate<Author> PublishedMatch= new Predicate<Author>() {
+
+            @Override
+            public boolean test(Author t) {
+                return t.books.stream().anyMatch(new Predicate<Book>() {
+
+                    @Override
+                    public boolean test(Book b) {
+                        return b.published;
+                    }
+                    
+                });
+            }
+            
+        };
+        authors.stream()
+        .filter(p1)
+        .filter(PublishedMatch)
+        .forEach(System.out::println);
+
+        banner("Active authors that have at least one published book - lambda");
+        // TODO With functional interfaces used directly  >>>Done
+
+        authors.stream()
+        .filter(author->author.active)
+        .filter(author->author.books.stream().anyMatch(x->x.published))
+        .forEach(System.out::println);
+
+
+    }
+
+    private static void banner(final String m) {
+        System.out.println("#### " + m + " ####");
+    }
+}
+
+class Library {
+    public static List<Author> getAuthors() {
+        return Arrays.asList(
+                new Author("Author A", true, Arrays.asList(
+                        new Book("A1", 100, true),
+                        new Book("A2", 200, true),
+                        new Book("A3", 220, true))),
+                new Author("Author B", true, Arrays.asList(
+                        new Book("B1", 80, true),
+                        new Book("B2", 80, false),
+                        new Book("B3", 190, true),
+                        new Book("B4", 210, true))),
+                new Author("Author C", true, Arrays.asList(
+                        new Book("C1", 110, true),
+                        new Book("C2", 120, false),
+                        new Book("C3", 130, true))),
+                new Author("Author D", false, Arrays.asList(
+                        new Book("D1", 200, true),
+                        new Book("D2", 300, false))),
+                new Author("Author X", true, Collections.emptyList()));
+    }
+}
+
+class Author {
+    String name;
+    boolean active;
+    List<Book> books;
+
+    Author(String name, boolean active, List<Book> books) {
+        this.name = name;
+        this.active = active;
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\t| " + (active ? "Active" : "Inactive");
+    }
+}
+
+class Book {
+    String name;
+    int price;
+    boolean published;
+
+    Book(String name, int price, boolean published) {
+        this.name = name;
+        this.price = price;
+        this.published = published;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\t| " + "\t| $" + price + "\t| " + (published ? "Published" : "Unpublished");
+    }
+}
